47ac2d93b6a7cc3ca57dcf8cd29c28c05ad855bc
onionbulbperception <ezpr.flip303@passinbox.com>
2025-03-03 05:28:01 +0200

    Player's movement animations

--- scripts/player.gd
+++ scripts/player.gd
@@ -2,11 +2,13 @@ extends CharacterBody2D
 ## Simple movement script for player's character.
 ##
 ## Script provides a basic framework for character movement using gravity
-## handling, jumping, horizontal movement and applying velocity and slide.
+## handling, jumping, horizontal movement and applying velocity. Also script
+## handles playin player's animations based on input.
 
 
 const SPEED = 125.0
 const JUMP_VELOCITY = -250.0
+@onready var animated_sprite = $AnimatedSprite2D
 
 
 func _physics_process(delta: float) -> void:
@@ -15,12 +17,27 @@ func _physics_process(delta: float) -> void:
 		velocity += get_gravity() * delta
 
 	# Jumping
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
 	# Horizontal movement
-	# TODO: Replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
+	var direction := Input.get_axis("move_left", "move_right")
+	
+	# Sprite flipping
+	if direction > 0:
+		animated_sprite.flip_h = false
+	elif direction < 0:
+		animated_sprite.flip_h = true
+	
+	# Play animations
+	if is_on_floor():
+		if direction == 0:
+			animated_sprite.play("idle")
+		else:
+			animated_sprite.play("run")
+	else:
+		animated_sprite.play("jump")
+
 	if direction:
 		velocity.x = direction * SPEED
 	else:
